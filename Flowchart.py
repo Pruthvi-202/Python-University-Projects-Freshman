76ed447a196a126aa6dc2efaa3754b899732cdcb
Pruthvi-202 <137182785+Pruthvi-202@users.noreply.github.com>
2024-12-23 14:15:49 -0500

    Add files via upload

---
+++ Flowchart.py
@@ -0,0 +1,24 @@
+x = input("Input one number: ")
+x = float(x)
+y = input("Input another number: ")
+y = float(y)
+difference = x - y
+print("The answer is:", difference)
+
+physique = "Aquaman" or "Saitama"
+
+if physique == "Saitama":
+   for i in ["Monday","Tuesday","Wednesday","Thursday","Friday"]:
+       print(i,":")
+       print("100 Push-ups")
+       print("100 Sit-ups")
+       print("1 Meter Run")
+elif physique == "Aquaman":
+    for i in ["Monday","Tuesday","Wednesday","Thursday","Friday"]:
+        print(i,":")
+        print("500 Strides")
+        print("10 Mile Run")
+
+
+for i in range(100):
+    print("Go Cougars!!!")
